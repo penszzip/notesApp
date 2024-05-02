b26543ac1b32ef6762b88da6b942e343bf7f67cb
penszzip <pazhiloyflex@gmail.com>
2024-05-02 01:05:32 -0400

    added notes component

---
+++ notesClient/src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const Notes = () => {
+    const [notes, setNotes] = useState([]);
+    
+    useEffect(() => {
+        const fetchNotes = async () => {
+            
+        }
+    })
+
+    return (
+        <div>Notes</div>
+    )
+}
+
+export default Notes
