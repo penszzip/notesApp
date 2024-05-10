551b10ac15fe4130c5fc9b3760e3f2aea0108ad3
penszzip <pazhiloyflex@gmail.com>
2024-05-09 23:31:16 -0400

    loading animation change

--- notesClient/src/components/skeletons/Skeleton.jsx
+++ notesClient/src/components/skeletons/Skeleton.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 
 const Skeleton = () => {
   return (
-    <div className='skeleton animate-pulse'>
-        <div className="text"></div>
-        <div className="footer"></div>
+    <div className='skeleton'>
+        <div className="skeleton-header animate-pulse"></div>
+        <div className="text animate-pulse"></div>
+        <div className="footer animate-pulse"></div>
     </div>
   )
 }
